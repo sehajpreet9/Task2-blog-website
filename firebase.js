9c36d427a92ed038811b6f76c9229e59584b6be6
sehajpreet9 <148732538+sehajpreet9@users.noreply.github.com>
2024-02-18 16:18:32 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyBPZaDH9fAcWDP-pTCTAgX4pwsFCbMimY0",
+    authDomain: "blogging-website-7a602.firebaseapp.com",
+    projectId: "blogging-website-7a602",
+    storageBucket: "blogging-website-7a602.appspot.com",
+    messagingSenderId: "391685822804",
+    appId: "1:391685822804:web:56683f37ad5046cf0b90c1"
+  };
+
+  
+  const app = initializeApp(firebaseConfig);
+  let db = firebase.firestore();
